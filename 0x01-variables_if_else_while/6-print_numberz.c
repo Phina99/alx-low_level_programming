bc36411e3f308deace0eb4fdb95de0447b3f0216
phina99 <catherinephina@gmail.comm>
2023-08-15 01:55:04 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *  main - main function
+ *
+ *  Return: always 0
+ */
+
+int main(void)
+{
+        int 1;
+
+	for (1 = 0 ; 1 < 10 ; 1++)
+        putchar(1 + '0');
+	putchar('\n');
+	return (0);
+}
