8b989c7f4502baeb437d6aa8641e9b9563e81cb3
phina99 <catherinephina@gmail.comm>
2023-08-16 12:18:46 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,7 +14,6 @@ void jack_bauer(void)
 	while (i < 24)
 	{
 		j = 0;
-
 		while (j < 60)
 		{
 			_putchar((i / 10) + '0');
@@ -23,7 +22,7 @@ void jack_bauer(void)
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 			_putchar('\n');
-			J++;
+			j++;
 		}
 		i++;
 	}
