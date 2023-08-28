a8b3ce3f7d804060f370837bd77bf46da8b3e02b
phina99 <catherinephina@gmail.comm>
2023-08-28 10:12:09 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,43 +1,39 @@
 #include "main.h"
 
 /**
- * _strstr - a function that locates a substring
+ * _strstr - Located a substring.
+ * @haystack: The string to be searched.
+ * @needle: The substring to be located.
  *
- * @haystack: input string to search for matching
- *            substrings
- * @needle: subtring to search for
- *
- * Return: a pointer to the beginning
- *         of the located substring or
- *         NULL if substring is not found
+ * Return: If the substring is located - a pointer to the beginning
+ *                                       of the located substring.
+ * If the substring is not located - NULL.
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-/**
- * we initialize a helping variable
- * to assist in returning one of
- * our parameters pointers haystack
- *
-*/
-char *h;
-char *h, *n;
+	int index;
 
-while (*haystack != '\0')
-{
-h = haystack;
-while (*haystack != '\0' && *needle != '\0' && *haystack == *needle)
-n = needle;
-while (*n != '\0' && *haystack == *n)
-{
-haystack++;
-needle++;
-n++;
-}
-if (!*needle)
-if (!*n)
-return (h);
-haystack++;
-}
-return ('\0');
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		index = 0;
+
+		if (haystack[index] == needle[index])
+		{
+			do {
+				if (needle[index + 1] == '\0')
+					return (haystack);
+
+				index++;
+
+			} while (haystack[index] == needle[index]);
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
 }
