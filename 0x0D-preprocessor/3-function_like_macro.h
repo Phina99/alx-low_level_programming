222824ca632b846adece43a564356769324b28af
phina99 <catherinephina@gmail.comm>
2023-09-11 04:45:47 -0700

    4-sum.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef LIKEMACRO_H
+#define LIKEMACRO_H
+/**
+ * file: 3-function_like_macro
+ * @x: input
+ * Desc: header file
+ */
+#define ABS(x)((x) < 0 ? -(1) * (x) : (x))
+
+#endif
---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * file: main
+ *
+ * Desc: header file
+ */
+int _putcha(char c);
+
+#endif
